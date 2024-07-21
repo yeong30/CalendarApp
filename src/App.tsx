a825365f259a0b5d831b832afa7367a056ca2794
uuyeong <proyeongs@gmail.com>
2024-07-21 23:51:27 +0900

    chore: path alias 설정

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, useColorScheme} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={backgroundStyle}></ScrollView>
+    </SafeAreaView>
+  );
+}
+
+export default App;
